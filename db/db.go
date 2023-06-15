50ebab97087f745dca212953c9205191d4fbffec
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-06-15 18:05:14 -0300

    started

---
+++ db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func InitDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		println("Error connecting to database")
+		log.Panic(err)
+	}
+	DB = db
+}
